6711421f77a73bcedef676055c9f6e665f93657f
MargoBil <margo10011990@gmail.com>
2020-07-24 11:20:26 +0300

    Updates

---
+++ precache-manifest.864de3269a1045eedd081f3ca16c51c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2788d440f1196fdb6e995465bbc2ea",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "a4a6014ee9fa89ceb007",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.abbc8899.chunk.css"
+  },
+  {
+    "revision": "03a1ce11151045b848e7",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.a6e25aa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.a6e25aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a6014ee9fa89ceb007",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.f65d3f50.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
